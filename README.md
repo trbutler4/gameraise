3bed5f6e1c4e376a806dfd7692f701908c5ad2e4
Thomas Butler <trbiv@proton.me>
2024-11-04 20:53:39 -0600

    minimal login example

--- README.md
+++ README.md
@@ -5,6 +5,5 @@
 
 <img width="1634" alt="Screenshot 2024-11-03 at 5 50 11 PM" src="https://github.com/user-attachments/assets/b5ac458f-cef5-4700-b8b3-09dfaa975de6">
 
-## License
-This project includes code from https://github.com/tkhq/demo-embedded-wallet (Copyright (c) 2024 Turnkey) licensed under the MIT License.
-See the [LICENSE](LICENSE) file for details.
+## License Information
+This project includes modified code from https://github.com/tkhq/demo-embedded-wallet (Copyright (c) 2024 Turnkey) licensed under the MIT License.
--- gameraise-app/src/env.mjs
+++ gameraise-app/src/env.mjs
@@ -3,11 +3,27 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  client: {},
-  server: {},
-  runtimeEnv: {},
+  client: {
+    NEXT_PUBLIC_ORGANIZATION_ID: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().optional(),
+    NEXT_PUBLIC_BASE_URL: z.string().min(1),
+    NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1),
+  },
+  server: {
+    TURNKEY_API_PUBLIC_KEY: z.string().min(1),
+    TURNKEY_API_PRIVATE_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    TURNKEY_API_PUBLIC_KEY: process.env.TURNKEY_API_PUBLIC_KEY,
+    TURNKEY_API_PRIVATE_KEY: process.env.TURNKEY_API_PRIVATE_KEY,
+    NEXT_PUBLIC_ORGANIZATION_ID: process.env.NEXT_PUBLIC_ORGANIZATION_ID,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+  },
   extends: [vercel()],
 })
+
 /*
 export const env = createEnv({
   client: {
