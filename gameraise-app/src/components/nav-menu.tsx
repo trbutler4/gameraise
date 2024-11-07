ac47910ca61c5fc9ad8e35f438b2c083bf684ffc
Thomas Butler <trbiv@proton.me>
2024-11-06 21:34:09 -0600

    adding logo

--- gameraise-app/src/components/nav-menu.tsx
+++ gameraise-app/src/components/nav-menu.tsx
@@ -1,13 +1,15 @@
+import Image from "next/image"
 import Link from "next/link"
 
+import gameraiseLogo from "../../public/gameraise_logo 3.png"
 import Account from "./account"
 import { Icons } from "./icons"
 
 export default function NavMenu() {
   return (
-    <div className=" flex h-[5rem] items-center justify-between gap-4 p-4 sm:px-10">
+    <div className=" flex h-[5rem] items-center justify-between gap-4 bg-secondary p-4 sm:px-10">
       <Link href="/home">
-        <Icons.turnkey className="h-6 w-auto  fill-white stroke-none sm:h-7" />
+        <Image src={gameraiseLogo} alt="gameraise logo" />
       </Link>
       <div className="flex items-center justify-center gap-4">
         <Account />
