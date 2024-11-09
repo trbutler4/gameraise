39b67da6c3bdb2b3325aa6b99deacefa26864689
Thomas <trbiv@proton.me>
2024-11-09 03:40:49 -0600

    set up arbitrum mainnet fork

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,18 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+  },
 };
 
 export default config;
